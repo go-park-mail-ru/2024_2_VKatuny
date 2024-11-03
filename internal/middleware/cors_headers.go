7820da52370420fa1122e44058a8a3ccf201b884
Ifelsik <misha2004546@gmail.com>
2024-11-03 14:34:14 +0300

    feature: allowed method checker middleware added

--- internal/middleware/cors_headers.go
+++ internal/middleware/cors_headers.go
@@ -4,18 +4,17 @@ import (
 	"net/http"
 )
 
-// SetSecurityAndOptionsHeaders Accepts funcion next and sets up CORS and content-type headers
+// SetSecurityAndOptionsHeaders Accepts function next and sets up CORS and content-type headers
 // Returns wrapped function next with headers
-func SetSecurityAndOptionsHeaders(next http.Handler, hostWithSchema string) http.Handler {
+func SetSecurityAndOptionsHeaders(next http.Handler, frontURI string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Set up CORS headers
-		w.Header().Set("Access-Control-Allow-Origin", hostWithSchema)
+		w.Header().Set("Access-Control-Allow-Origin", frontURI)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Accept")
 
 		if r.Method == http.MethodOptions {
-			// should i return http.StatusOK?
 			return
 		}
 		// Set up content-type header
--- internal/middleware/universalMarshal.go
+++ internal/middleware/universalMarshal.go
@@ -7,11 +7,11 @@ import (
 )
 
 // UniversalMarshal marshal any struct to json (use it for any answer from handlers).
-// Writes http status to http.ResposeWriter
+// Writes http status to http.ResponseWriter
 func UniversalMarshal(w http.ResponseWriter, status int, body interface{}) error {
 	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(body); err != nil {
-		return fmt.Errorf("Err while marshal")
+		return fmt.Errorf("err while marshal")
 	}
 	return nil
 }
