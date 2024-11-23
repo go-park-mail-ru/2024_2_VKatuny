cc868f0789f8f2d8f63cfad679472fea8a9eb555
Olg Muzalev <ruzrushitelyvselennoy@mail.ru>
2024-11-23 16:32:46 +0300

    fix: getquestions

--- microservices/survey/gerenator/servery.go
+++ microservices/survey/gerenator/servery.go
@@ -1,1 +1,31 @@
 package servery
+
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+
+	"github.com/go-park-mail-ru/lectures/8-microservices/4_grpc/Survey"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+
+	"context"
+)
+
+
+type SurveyManager struct {
+	Survey.UnimplementedAuthCheckerServer
+}
+
+func NewSuveyService() *SurveyManager {
+	return &SurveyManager{
+	}
+}
+
+func (sm *SurveyManager) GetAuthorization(GetAuthorizationInput) returns (GetAuthorizationOutput) {
+	authorized := GetAuthorizationOutput{}
+	return authorized
+}
