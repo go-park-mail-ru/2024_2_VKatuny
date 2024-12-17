6a91e80487eef560aab7f3e7d92f44fc388579e1
Ifelsik <misha2004546@gmail.com>
2024-12-17 12:57:27 +0300

    cv repository log made

--- internal/pkg/cvs/cvs.go
+++ internal/pkg/cvs/cvs.go
@@ -1,25 +1,27 @@
 package cvs
 
 import (
+	"context"
+
 	"github.com/go-park-mail-ru/2024_2_VKatuny/internal/pkg/dto"
 )
 
 // Здесь все интерфейсы взаимодействия с CVs
 
 type ICVsRepository interface {
-	GetCVsByApplicantID(applicantID uint64) ([]*dto.JSONCv, error)
-	Create(cv *dto.JSONCv) (*dto.JSONCv, error)
-	GetByID(ID uint64) (*dto.JSONCv, error)
-	Update(ID uint64, updatedCv *dto.JSONCv) (*dto.JSONCv, error)
-	Delete(ID uint64) error
-	SearchAll(offset uint64, num uint64, searchStr, group, searchBy string) ([]*dto.JSONCv, error)
+	GetCVsByApplicantID(ctx context.Context, applicantID uint64) ([]*dto.JSONCv, error)
+	Create(ctx context.Context, cv *dto.JSONCv) (*dto.JSONCv, error)
+	GetByID(ctx context.Context, ID uint64) (*dto.JSONCv, error)
+	Update(ctx context.Context, ID uint64, updatedCv *dto.JSONCv) (*dto.JSONCv, error)
+	Delete(ctx context.Context, ID uint64) error
+	SearchAll(ctx context.Context, offset uint64, num uint64, searchStr, group, searchBy string) ([]*dto.JSONCv, error)
 }
 
 type ICVsUsecase interface {
-	GetApplicantCVs(applicantID uint64) ([]*dto.JSONGetApplicantCV, error)
-	CreateCV(cv *dto.JSONCv, currentUser *dto.UserFromSession) (*dto.JSONCv, error)
-	GetCV(ID uint64) (*dto.JSONCv, error)
-	UpdateCV(ID uint64, currentUser *dto.UserFromSession, cv *dto.JSONCv) (*dto.JSONCv, error)
-	DeleteCV(ID uint64, currentUser *dto.UserFromSession) error
-	SearchCVs(offsetStr, numStr, searchStr, group, searchBy string) ([]*dto.JSONGetApplicantCV, error)
+	GetApplicantCVs(ctx context.Context, applicantID uint64) ([]*dto.JSONGetApplicantCV, error)
+	CreateCV(ctx context.Context, cv *dto.JSONCv, currentUser *dto.UserFromSession) (*dto.JSONCv, error)
+	GetCV(ctx context.Context, ID uint64) (*dto.JSONCv, error)
+	UpdateCV(ctx context.Context, ID uint64, currentUser *dto.UserFromSession, cv *dto.JSONCv) (*dto.JSONCv, error)
+	DeleteCV(ctx context.Context, ID uint64, currentUser *dto.UserFromSession) error
+	SearchCVs(ctx context.Context, offsetStr, numStr, searchStr, group, searchBy string) ([]*dto.JSONGetApplicantCV, error)
 }
